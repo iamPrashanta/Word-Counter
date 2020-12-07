86b4faecd25315ebca6352eba37acb3e409189cc
Prashanta mondal <iloveprashanta@gmail.com>
2020-12-07 13:22:26 +0530

    update

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 
 function getInfo() {
     let str = document.getElementById("inputbox");
+    let detecter = document.getElementById("lang_detecter");
+    let lang_html = document.getElementById("lang_html");
+    let lang_php = document.getElementById("lang_php");
+    let lang_javascript = document.getElementById("lang_javascript");
 
     let word_w = document.getElementById("w_count");
     let word_wc = document.getElementById("wc_text");
-
-    let space_s = document.getElementById("s_count");
-    let space_sc = document.getElementById("sc_text");
 
     let character_c = document.getElementById("c_count");
     let character_cc = document.getElementById("cc_text");
@@ -14,8 +15,13 @@ function getInfo() {
     let paraghaph_p = document.getElementById("p_count");
     let paraghaph_pc = document.getElementById("pc_text");
 
-    let sentence_s = document.getElementById("sen_count");
+    let sentence_sc = document.getElementById("sen_count");
     let sentence_sen = document.getElementById("sen_text");
+
+    let reg_html = /(<html|<\/html|html>)/gim;
+    let reg_javascript = /(<script>|((var)|(let)|(const)|(function\(?\)?)|\(\)))/gim;
+    let reg_php = /((<\?php|\?>))/gim;
+
     //word count
     let wordCount = str.value.match(/[a-z]{0,}\w+/gim);
     word_w.innerHTML = (wordCount.length);
@@ -24,17 +30,9 @@ function getInfo() {
     } else {
         word_wc.innerHTML = " Word";
     }
-    //space count
-    let spaceCount = str.value.match(/(\s)/gim);
-    space_s.innerHTML = (spaceCount.length);
-    if (spaceCount.length > 1) {
-        space_sc.innerHTML = " Spaces";
-    } else {
-        space_sc.innerHTML = " Space";
-    }
 
     // //character count
-    let characterCount = str.value.match(/([A-Z]{0})\w/gim);
+    let characterCount = str.value.match(/[a-z0-9&~`'";:<>/\\\?|()*^%$#@!. ,]/gim);
     character_c.innerHTML = (characterCount.length);
     if (characterCount.length > 1) {
         character_cc.innerHTML = " Characters";
@@ -52,12 +50,38 @@ function getInfo() {
     }
 
     //sentence count
-    let sentenceCount = str.value.match(/[a-z0-9]\s*?(\.)/gim);
-    sentence_s.innerHTML = (sentenceCount.length);
+    let sentence_count = str.value.match(/[a-z0-9]\s*?(\.)/gim);
+    sentence_sc.innerText = (sentence_count.length);
     if (sentenceCount.length > 1) {
         sentence_sen.innerHTML = " Sentences";
     } else {
         sentence_sen.innerHTML = " Sentence";
+    }
+
+    //for language detections
+    let isHtmlCodeExisting = reg_html.test(str.value);
+    if (isHtmlCodeExisting) {
+        detecter.style.display = "block";
+        lang_html.style.display = "contents";
+    } else {
+        lang_html.style.display = "none";
+    }
+    let isPhpCodeExisting = reg_php.test(str.value);
+    if (isPhpCodeExisting) {
+        detecter.style.display = "block";
+        lang_php.style.display = "contents";
+    } else {
+        lang_php.style.display = "none";
+    }
+    let isJavaScriptCodeExisting = reg_javascript.test(str.value);
+    if (isJavaScriptCodeExisting) {
+        detecter.style.display = "block";
+        lang_javascript.style.display = "contents";
+    } else {
+        lang_javascript.style.display = "none";
+    }
+    if (!isHtmlCodeExisting && !isPhpCodeExisting && !isJavaScriptCodeExisting) {
+        detecter.style.display = "none";
     }
 }
 
@@ -95,7 +119,25 @@ function sentencecase() {
 }
 
 
+//clear text field
+function cls() {
+    let str_field = document.getElementById("inputbox");
+    if (!str_field.value.length == 0) {
+        var conf_sms = confirm('are you sure ?');
+        if (conf_sms == true) {
+            str_field.value = "";
+        }
+    } else {
+        alert('Textfiels already Cleared');
+    }
+}
+
+
+
+
 //\b[a-z].*? //select first letters of all words
 //[a-zA-Z0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]* //select all words
 //["']?[A-Z][^.?!]+((?![.?!]['"]?\\s["']?[A-Z][^.?!]).)+[.?!'"]+ //select all paragraph
 //[A-Z][^\.][^\.]* //select every sentence
+//(<html|<\/html|html>) //html
+//
